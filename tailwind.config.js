9397b32d395adcd2cb38d1c0b6ba5ba35579f6a8
Fake Name <63071333+fakename131@users.noreply.github.com>
2023-08-12 11:28:23 -0400

    Upload FIles

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './src/**/*.{svelte,html,js,ts,css}'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('daisyui')
+    ],
+    daisyui: {
+        themes: false
+    }
+}
